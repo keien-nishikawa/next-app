a8d44866945de58d0a629dfbc361008a0c6e6dc7
keien-nishikawa <keien.nishikawa@clubx.jp>
2022-06-09 22:43:11 +0900

    first commit

---
+++ src/pages/[id]/style.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/css';
+
+export const container = css`
+  width: min(100%, 500px);
+  margin: 5% auto;
+  padding: 15px 20px;
+  border: 1px solid #5c93bb2b;
+  box-shadow: rgb(0 0 0 / 8%) 0px 6px 16px;
+  border-radius: 4px;
+`;
+
+export const title = css`
+  font-size: 16px;
+  font-weight: 700;
+  color: red;
+  padding: 2.5px 0;
+`;
+
+export const body = css`
+  font-size: 14px;
+  line-height: 1.5;
+  color: #333;
+  padding: 2.5px 0;
+`;
