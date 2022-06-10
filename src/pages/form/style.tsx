1b5a63f83683b1bb77e7c7225e1b0918ebfd11b4
keien-nishikawa <keien.nishikawa@clubx.jp>
2022-06-10 11:23:58 +0900

    wip

--- src/pages/form/style.tsx
+++ src/pages/form/style.tsx
@@ -2,6 +2,9 @@ import { css } from '@emotion/react';
 
 export const container = css`
   position: relative;
-  width: min-content(500px, 100%);
-  margin: 0 auto;
+  width: min(95% - 20px, 500px);
+  margin: 5% auto;
+  padding: 10px;
+  border: 1px solid #5c93bb2b;
+  box-shadow: rgb(0 0 0 / 8%) 0px 6px 16px;
 `;
