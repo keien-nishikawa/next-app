85c11d7a073da2ffb648b8df62367ffe0a61f5f5
keien-nishikawa <keien.nishikawa@clubx.jp>
2022-06-11 19:24:43 +0900

    wip

--- src/pages/404/index.page.tsx
+++ src/pages/404/index.page.tsx
@@ -1,13 +1,14 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 import * as style from './style';
 
 const Page: NextPage = () => (
   <div css={style.container}>
     <h1 css={style.code}>404</h1>
     <div css={style.message}>This page could not be found.</div>
-    <a href="/" css={style.appLink}>
-      NextAppに戻る
-    </a>
+    <Link href="/" passHref>
+      <a css={style.appLink}>NextAppに戻る</a>
+    </Link>
   </div>
 );
 
